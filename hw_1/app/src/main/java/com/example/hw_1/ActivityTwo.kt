f137fdf92e845bb9cbfc1e4eaea043158b798f2e
Mikhail Murtazaliev <volboy@yandex.ru>
2021-02-26 13:41:36 +0300

    hw1_BroadcastReceiver, ContentProvider

--- hw_1/app/src/main/java/com/example/hw_1/ActivityTwo.kt
+++ hw_1/app/src/main/java/com/example/hw_1/ActivityTwo.kt
@@ -1,62 +1,91 @@
 package com.example.hw_1
 
+import android.Manifest
 import android.app.Activity
 import android.app.IntentService
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.util.Log
-import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.example.hw_1.databinding.ActivityTwoBinding
 
+
 class ActivityTwo : AppCompatActivity() {
-    val contacts= arrayOf("")
+
     lateinit var localBroadcastReceiver: LocalBroadcastReceiver
-    lateinit var intentResult:Intent
+    lateinit var intentFilter: IntentFilter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         val binding =
             DataBindingUtil.setContentView<ActivityTwoBinding>(this, R.layout.activity_two)
-        localBroadcastReceiver= LocalBroadcastReceiver(contacts)
+
+        localBroadcastReceiver = LocalBroadcastReceiver()
+        intentFilter = IntentFilter(LOADED_CONTACTS_ACTION)
+        val localBroadcastManager = LocalBroadcastManager.getInstance(applicationContext)
+        localBroadcastManager.registerReceiver(localBroadcastReceiver, intentFilter)
 
         binding.btnLoadData.setOnClickListener {
-            val intentForService = Intent(this, LoadContactService::class.java)
-            startService(intentForService)
+                val intentForService = Intent(applicationContext, LoadContactService::class.java)
+                startService(intentForService)
         }
     }
+
     @Suppress("DEPRECATION")
     class LoadContactService() : IntentService("") {
         override fun onHandleIntent(intent: Intent?) {
-            //грузим контакты
-            Log.i("QWERTY", "YES")
-            /*val intentForReceiver=Intent("LOADED_CONTACT_ACTION")
-            LocalBroadcastManager.getInstance(this).sendBroadcast(intentForReceiver)*/
+            val contacts = getContacts()
+            Log.i("info_hw1", "Service was worked")
+            val intentForReceiver = Intent(LOADED_CONTACTS_ACTION)
+            intentForReceiver.putExtra(DATA_CONTACTS_KEY, contacts[1])
+            LocalBroadcastManager.getInstance(applicationContext).sendBroadcast(intentForReceiver)
+        }
+
+        private fun getContacts(): MutableList<String> {
+
+            val cursor =
+                contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null)
+            val contacts: MutableList<String> = mutableListOf("")
+
+            if (cursor != null) {
+                while (cursor.moveToNext()) {
+                    contacts.add(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME_PRIMARY)))
+                }
+                cursor.close()
+            }
+            return contacts
         }
     }
 
-     class LocalBroadcastReceiver(contacts_param:Array<String>): BroadcastReceiver(){
-        private val contacts=contacts_param
+    inner class LocalBroadcastReceiver() : BroadcastReceiver() {
+
         override fun onReceive(context: Context?, intent: Intent?) {
-            contacts.plus("Кря")
-            Toast.makeText(context, "Кря", Toast.LENGTH_SHORT).show()
-        }
-    }
+            val intentForMainActivity = Intent()
+            intentForMainActivity.putExtra(
+                DATA_CONTACTS_KEY,
+                intent?.getStringExtra(DATA_CONTACTS_KEY)
+            )
+            setResult(Activity.RESULT_OK, intentForMainActivity)
+            Log.i("info_hw1", "BroadcastReceiver was worked")
+            finish()
 
-    override fun onStart() {
-        super.onStart()
-        val intentFilter = IntentFilter("LOADED_CONTACT_ACTION")
-        val localBroadcastManager = LocalBroadcastManager.getInstance(this)
-        localBroadcastManager.registerReceiver(localBroadcastReceiver, intentFilter)
+        }
     }
 
     override fun onDestroy() {
-        LocalBroadcastManager.getInstance(this).unregisterReceiver(localBroadcastReceiver);
+        LocalBroadcastManager.getInstance(applicationContext)
+            .unregisterReceiver(localBroadcastReceiver);
         super.onDestroy()
 
     }
--- hw_1/app/src/main/java/com/example/hw_1/MainActivity.kt
+++ hw_1/app/src/main/java/com/example/hw_1/MainActivity.kt
@@ -1,31 +1,78 @@
 package com.example.hw_1
 
+import android.Manifest
+import android.Manifest.permission.READ_CONTACTS
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.example.hw_1.databinding.ActivityMainBinding
 
+const val REQUEST_CODE_ACTIVITY_TWO = 1
+const val REQUEST_CODE_PERMISSION = 1
+const val LOADED_CONTACTS_ACTION = "LOADED_CONTACT_ACTION"
+const val DATA_CONTACTS_KEY = "DATA_CONTACTS_KEY"
+
 class MainActivity : AppCompatActivity() {
-    private val requestCodeForActivityTwo = 1
+
+    var readContactGranted = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding =
             DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        getPermission()
         binding.btnToLoadActivity.setOnClickListener {
-            val intent = Intent(this, ActivityTwo::class.java)
-            startActivityForResult(intent, requestCodeForActivityTwo)
+            if (readContactGranted) {
+                val intent = Intent(this, ActivityTwo::class.java)
+                startActivityForResult(intent, REQUEST_CODE_ACTIVITY_TWO)
+            } else {
+                Toast.makeText(this, "Необходимо разрешить доступ к контактам", Toast.LENGTH_LONG)
+                    .show()
+                getPermission()
+            }
+        }
+    }
+
+    private fun getPermission() {
+        val readContactPermission =
+            ContextCompat.checkSelfPermission(
+                applicationContext,
+                READ_CONTACTS
+            );
+        if (readContactPermission == PackageManager.PERMISSION_GRANTED) {
+            readContactGranted = true
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(READ_CONTACTS),
+                REQUEST_CODE_PERMISSION
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == 1) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                readContactGranted = true;
+            }
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == requestCodeForActivityTwo) {
+        if (requestCode == REQUEST_CODE_ACTIVITY_TWO) {
             if (resultCode == Activity.RESULT_OK) {
-                val someData = data?.getStringExtra("SOME_DATA")
+                val someData = data?.getStringExtra(DATA_CONTACTS_KEY)
                 Toast.makeText(this, someData, Toast.LENGTH_LONG).show()
             }
         }
