fa6826962bfdbd4a9424b43783e947274042fc2d
Mikhail Murtazaliev <volboy@yandex.ru>
2021-03-15 08:50:55 +0300

    Without folder hw_2

---
+++ course_project/app/src/main/java/com/volboy/hw_2/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.volboy.hw_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun clickEmojiiView(view: View) {
+        //view.isSelected = !view.isSelected
+    }
+}
