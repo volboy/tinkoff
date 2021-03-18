8060b7b7fa17e9a583fc53671861e8053062054e
Mikhail Murtazaliev <volboy@yandex.ru>
2021-03-18 15:16:30 +0300

    Логика добавления реакций

--- course_project/app/src/main/java/com/volboy/course_project/MainActivity.kt
+++ course_project/app/src/main/java/com/volboy/course_project/MainActivity.kt
@@ -3,21 +3,24 @@ package com.volboy.course_project
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.view.KeyEvent
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.volboy.course_project.databinding.ActivityMainBinding
 import com.volboy.course_project.message_recycler_view.*
 import com.volboy.course_project.model.LoaderMessage
+import com.volboy.course_project.model.Reaction
+import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity(), EmojiBottomFragment.EmojiEventInterface {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var reactions: MutableList<Reaction>
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val emj = String(Character.toChars(0x1F60E))
+        reactions = mutableListOf(Reaction(emj, "You", 1))
+
         val longClickListener: (View) -> Boolean = {
             EmojiBottomFragment().apply {
                 show(supportFragmentManager, EmojiBottomFragment.TAG)
@@ -30,14 +33,10 @@ class MainActivity : AppCompatActivity(), EmojiBottomFragment.EmojiEventInterfac
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.fragmentMessages.recyclerMessage.layoutManager =
-            LinearLayoutManager(applicationContext)
-
+        binding.fragmentMessages.recyclerMessage.layoutManager = LinearLayoutManager(applicationContext)
         binding.fragmentMessages.recyclerMessage.adapter = messageAdapter
         messageAdapter.items = loaderMessage.remoteMessage()
         binding.fragmentMessages.recyclerMessage.scrollToPosition(messageAdapter.items.size - 1)
-
         binding.fragmentMessages.messageBtn.setOnClickListener {
             messageAdapter.items = loaderMessage.addMessage(getNewMessage())
             binding.fragmentMessages.recyclerMessage.scrollToPosition(messageAdapter.items.size - 1)
@@ -45,7 +44,6 @@ class MainActivity : AppCompatActivity(), EmojiBottomFragment.EmojiEventInterfac
 
         binding.fragmentMessages.messageBox.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {}
-
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
@@ -68,6 +66,27 @@ class MainActivity : AppCompatActivity(), EmojiBottomFragment.EmojiEventInterfac
     }
 
     override fun emojiClickListener(emoji: String) {
-        Toast.makeText(this, emoji, Toast.LENGTH_SHORT).show()
+        var r = reactions.firstOrNull { it.emoji == emoji }
+        if (r == null) {
+            reactions.add(Reaction(emoji, "You", 1))
+        } else {
+            if (r.usersId == "You") {
+                r.count = r.count - 1
+                if (r.count <= 0) {
+                    r.count = 0
+                }
+            } else {
+                r.count = r.count + 1
+            }
+        }
+        var iterator = reactions.listIterator()
+        while (iterator.hasNext()) {
+            var item = iterator.next()
+            if (item.count == 0) {
+                iterator.remove()
+            }
+        }
+        Toast.makeText(this, reactions.toString(), Toast.LENGTH_LONG).show()
     }
 }
+
--- course_project/app/src/main/java/com/volboy/course_project/model/Reaction.kt
+++ course_project/app/src/main/java/com/volboy/course_project/model/Reaction.kt
@@ -1,3 +1,8 @@
 package com.volboy.course_project.model
 
-class Reaction (val Emoji:String, val used_id:Int)
+class Reaction(val emoji: String, val usersId: String, var count:Int ) {
+    override fun toString(): String {
+        return "Reaction(emoji='$emoji', usersId='$usersId', count=$count)"
+    }
+
+}
