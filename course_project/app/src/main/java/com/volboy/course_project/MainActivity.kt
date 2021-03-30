2a731210d08f544a6296a624b4448ee36c43a7f3
Mikhail Murtazaliev <volboy@yandex.ru>
2021-03-30 10:24:32 +0300

    поиск в фрагменте All streams

--- course_project/app/src/main/java/com/volboy/course_project/MainActivity.kt
+++ course_project/app/src/main/java/com/volboy/course_project/MainActivity.kt
@@ -10,12 +10,10 @@ import io.reactivex.Observable
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val mainFragment = MainFragment()
-    private lateinit var searchText: Observable<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        toolBarSearch()
         val searchToolbar = binding.searchToolbar
         setSupportActionBar(searchToolbar)
         val transaction = supportFragmentManager.beginTransaction()
@@ -24,22 +22,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
     }
 
-    private fun toolBarSearch() = binding.searchEditText.addTextChangedListener { text ->
-        searchText = Observable.create { emitter ->
-            emitter.onNext(text.toString())
-        }
-       /* searchText
-            .filter { inputText -> inputText.isNotEmpty() }
-            .distinctUntilChanged()
-            .subscribe(
-                { inputText ->
-                    val filteredUsers = mutableUsers.filter { user -> user.name.contains(inputText) || user.surname.contains(inputText) }
-                    usersAdapter.update(filteredUsers)
-                },
-                { error -> showError(error) },
-                { onCompleteLog() }
-            )*/
-    }
+
 }
 
 
--- course_project/app/src/main/java/com/volboy/course_project/ui/channel_fragments/tab_layout_fragments/StreamsFragment.kt
+++ course_project/app/src/main/java/com/volboy/course_project/ui/channel_fragments/tab_layout_fragments/StreamsFragment.kt
@@ -4,22 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import com.google.android.material.snackbar.Snackbar
 import com.volboy.course_project.R
 import com.volboy.course_project.databinding.FragmentStreamsBinding
-import com.volboy.course_project.databinding.FragmentSubscribedBinding
 import com.volboy.course_project.message_recycler_view.CommonAdapter
 import com.volboy.course_project.message_recycler_view.ViewTyped
 import com.volboy.course_project.model.ObservableStreams
-import com.volboy.course_project.ui.channel_fragments.MessagesFragment
-import java.io.Serializable
+import io.reactivex.Observable
 
 class StreamsFragment : Fragment(), UiHolderFactory.ChannelsInterface {
     private lateinit var binding: FragmentStreamsBinding
     private val loaderStreams = ObservableStreams()
     private var listStreams = listOf<ViewTyped>()
     private lateinit var commonAdapter: CommonAdapter<ViewTyped>
+    private lateinit var searchText: Observable<String>
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentStreamsBinding.inflate(inflater, container, false)
@@ -38,6 +41,35 @@ class StreamsFragment : Fragment(), UiHolderFactory.ChannelsInterface {
         )
         commonAdapter.items = listStreams
         binding.rwAllStreams.adapter = commonAdapter
+        val mActionBar = (requireActivity() as AppCompatActivity).supportActionBar;
+        mActionBar?.show()
+        val searchEdit = requireActivity().findViewById<EditText>(R.id.searchEditText)
+        searchEdit.addTextChangedListener { text ->
+            searchText = Observable.create { emitter ->
+                emitter.onNext(text.toString())
+            }
+            searchText
+                .filter { inputText -> inputText.isNotEmpty() }
+                .filter { inputText -> inputText[0]!=' '}
+                .distinctUntilChanged()
+                .subscribe(
+                    { inputText ->
+                        val filteredStreams = listStreams.filter { stream ->
+                            val item = stream as TitleUi
+                            item.title.contains(inputText)
+                        }
+                        if (filteredStreams.isEmpty()) {
+                            commonAdapter.items = listOf(TitleUi("Ничего не найдено", null, null, 0, R.layout.item_collapse, ""))
+                        } else {
+                            commonAdapter.items = filteredStreams
+                        }
+                    },
+                    { error -> Toast.makeText(requireContext(), error.message, Toast.LENGTH_SHORT).show() }
+                )
+            if (text.isNullOrEmpty()){
+                commonAdapter.items = listStreams
+            }
+        }
     }
 
     override fun getClickedView(view: View, position: Int, viewType: Int) {
